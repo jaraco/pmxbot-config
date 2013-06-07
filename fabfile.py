f4bad525e0a5872e8082e0881ac477dec7aca387
Jason R. Coombs <jaraco@jaraco.com>
2013-06-07 12:41:38 -0400

    Fleshed out the entire install process, including a bootstrap task.

--- fabfile.py
+++ fabfile.py
@@ -28,3 +28,46 @@ def install_python(version='3.3.2'):
 		run('make')
 		sudo('make install')
 	sudo('rm -Rf Python-{version}'.format(**vars()))
+
+@api.task
+def install_setuptools():
+	sudo('wget https://bitbucket.org/pypa/setuptools/raw/0.7.1/ez_setup.py -O - | python3.3')
+	sudo('rm setuptools*')
+
+packages = ' '.join([
+	'pmxbot',
+	'pmxbot-wolframalpha',
+	'jaraco.pmxbot',
+	'pymongo',
+	'chucknorris',
+	'pmxbot-haiku',
+])
+
+@api.task
+def install_pmxbot():
+	"Install pmxbot into a PEP-370 env at /usr/local/pmxbot"
+	sudo('mkdir -p /usr/local/pmxbot/lib/python3.3/site-packages')
+	sudo('PYTHONUSERBASE=/usr/local/pmxbot easy_install-3.3 --user '
+		+ packages)
+
+@api.task
+def install_supervisor():
+	sudo('aptitude install -y supervisor')
+	files.upload_template('supervisor.conf',
+		'/etc/supervisor/conf.d/pmxbot.conf')
+	sudo('supervisorctl reload')
+
+@api.task
+def update_pmxbot():
+	sudo('PYTHONUSERBASE=/usr/local/pmxbot easy_install-3.3 --user -U '
+		+ packages)
+	sudo('supervisorctl restart pmxbot')
+	sudo('supervisorctl restart pmxbotweb')
+
+@api.task
+def bootstrap():
+	install_config()
+	install_python()
+	install_setuptools()
+	install_supervisor()
+	install_pmxbot()
