92914dfbfd9192de8627a83793f0b6118a9f0607
Jason R. Coombs <jaraco@jaraco.com>
2013-10-01 12:06:21 -0700

    Use apt package for installing Python
    --HG--
    extra : amend_source : 57d7a2fbcaa16dfce0cc261e2bf4602e250e0444

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@ import getpass
 
 from fabric.contrib import files
 from fabric import api
-from fabric.api import sudo, run, cd
+from fabric.api import sudo, run
 
 @api.task
 def install_config():
@@ -16,18 +16,9 @@ def install_config():
 		use_sudo=True)
 
 @api.task
-def install_python(version='3.3.2'):
+def install_python():
 	sudo('aptitude update')
-	sudo('aptitude build-dep -y python')
-	sudo('aptitude install -y libssl-dev libsqlite3-dev libreadline-dev libbz2-dev')
-	url = ('http://www.python.org'
-		'/ftp/python/{version}/Python-{version}.tgz').format(**vars())
-	run('wget {url} -O - | tar xz'.format(url=url))
-	with cd('Python-{version}'.format(**vars())):
-		run('./configure')
-		run('make')
-		sudo('make install')
-	sudo('rm -Rf Python-{version}'.format(**vars()))
+	sudo('aptitude install python3')
 
 @api.task
 def install_setuptools():
