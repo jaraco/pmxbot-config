0cc56a0d741f0a446cd7c60432d5f069061d3572
Jason R. Coombs <jaraco@jaraco.com>
2018-03-12 09:00:47 -0400

    Feed the hobgoblins (delint).

--- fabfile.py
+++ fabfile.py
@@ -15,6 +15,7 @@ env.hosts = ['.'.join((host, domain))]
 
 python = 'python3.6'
 
+
 @api.task
 def install_config():
 	db_pass = getpass.getpass('MongoDB password for pmxbot [skip]> ')
@@ -22,25 +23,33 @@ def install_config():
 	google_trans_key = getpass.getpass('Google Translate key [skip]> ')
 	wolframalpha_key = getpass.getpass('Wolfram|Alpha key [skip]> ')
 	sudo('mkdir -p /etc/pmxbot')
-	files.upload_template('pmxbot.conf', '/etc/pmxbot/main.conf',
+	files.upload_template(
+		'pmxbot.conf', '/etc/pmxbot/main.conf',
 		use_sudo=True)
-	files.upload_template('web.conf', '/etc/pmxbot/web.conf',
+	files.upload_template(
+		'web.conf', '/etc/pmxbot/web.conf',
 		use_sudo=True)
 	if not files.exists('/etc/pmxbot/server.conf'):
-		files.upload_template('server.conf', '/etc/pmxbot/server.conf',
+		files.upload_template(
+			'server.conf', '/etc/pmxbot/server.conf',
 			use_sudo=True)
 	if db_pass or not files.exists('/etc/pmxbot/database.conf'):
-		files.upload_template('database.conf', '/etc/pmxbot/database.conf',
+		files.upload_template(
+			'database.conf', '/etc/pmxbot/database.conf',
 			context=dict(password=db_pass), use_sudo=True, mode=0o600)
 	if twilio_token or not files.exists('/etc/pmxbot/twilio.conf'):
-		files.upload_template('twilio.conf', '/etc/pmxbot/twilio.conf',
+		files.upload_template(
+			'twilio.conf', '/etc/pmxbot/twilio.conf',
 			context=dict(token=twilio_token), use_sudo=True, mode=0o600)
 	if google_trans_key or not files.exists('/etc/pmxbot/trans.conf'):
-		files.upload_template('trans.conf', '/etc/pmxbot/trans.conf',
+		files.upload_template(
+			'trans.conf', '/etc/pmxbot/trans.conf',
 			context=dict(key=google_trans_key), use_sudo=True, mode=0o600)
 	if wolframalpha_key or not files.exists('/etc/pmxbot/wolframalpha.conf'):
-		files.upload_template('wolframalpha.conf', '/etc/pmxbot/wolframalpha.conf',
+		files.upload_template(
+			'wolframalpha.conf', '/etc/pmxbot/wolframalpha.conf',
 			context=dict(key=wolframalpha_key), use_sudo=True, mode=0o600)
+
 
 @api.task
 def install_python():
@@ -50,6 +59,7 @@ def install_python():
 	with shell_env(**install_env):
 		tmpl = 'wget https://bootstrap.pypa.io/get-pip.py -O - | {python} - --user'
 		sudo(tmpl.format_map(globals()))
+
 
 packages = ' '.join([
 	'pmxbot[irc,mongodb,viewer]',
@@ -71,6 +81,7 @@ install_env = dict(
 	PYTHONUSERBASE=install_root,
 )
 
+
 @api.task
 def install_pmxbot():
 	"Install pmxbot into a PEP-370 env at install_root"
@@ -78,9 +89,11 @@ def install_pmxbot():
 	with shell_env(**install_env):
 		sudo(tmpl.format_map(globals()))
 
+
 @api.task
 def install_systemd_service():
-	files.upload_template('pmxbot.service',
+	files.upload_template(
+		'pmxbot.service',
 		'/etc/systemd/system',
 		context=globals(),
 		use_sudo=True,
@@ -88,17 +101,21 @@ def install_systemd_service():
 	sudo('systemctl restart pmxbot')
 	sudo('systemctl enable pmxbot')
 
+
 @api.task
 def install_systemd_web_service():
-	files.upload_template('web.conf', '/etc/pmxbot/web.conf',
+	files.upload_template(
+		'web.conf', '/etc/pmxbot/web.conf',
 		use_sudo=True)
-	files.upload_template('pmxbot.web.service',
+	files.upload_template(
+		'pmxbot.web.service',
 		'/etc/systemd/system',
 		context=globals(),
 		use_sudo=True,
 	)
 	sudo('systemctl restart pmxbot.web')
 	sudo('systemctl enable pmxbot.web')
+
 
 @api.task
 def update_pmxbot():
@@ -107,6 +124,7 @@ def update_pmxbot():
 		sudo(tmpl.format_map(globals()))
 	sudo('systemctl restart pmxbot')
 	sudo('systemctl restart pmxbot.web')
+
 
 @api.task
 def ensure_fqdn():
@@ -119,6 +137,7 @@ def ensure_fqdn():
 	cmd = 'sed -i -e "s/{hostname}/{hostname}.{domain} {hostname}/g" /etc/hosts'
 	cmd = cmd.format(hostname=hostname, domain=domain)
 	sudo(cmd)
+
 
 @api.task
 def bootstrap():
